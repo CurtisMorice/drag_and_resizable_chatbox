a59ec19408df3351f4de534df19c8795ee30b6b6
CurtisM <curtismorice@gmail.com>
2020-09-12 09:49:38 -0500

    Chatbox moving and resizing

--- src/App.js
+++ src/App.js
@@ -1,26 +1,59 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Rnd } from 'react-rnd';
+import Logo from './logo-shield.png'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+const ChatBox = () => (
+  <div className="outer-container">
+    <div className="inner-wrapper">
+      <div className="content">
+        <div className="response-box-left">
+          <img src={Logo} draggable="false" alt="capella-logo"/>
+          <p className="p-left">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+            Aenean efficitur sit amet massa fringilla egestas.
+            Nullam condimentum luctus turpis.
+          </p>
+        </div>
+        <div className="response-box-right">
+          <p className="p-right">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+            Aenean efficitur sit amet massa fringilla egestas.
+            Nullam condimentum luctus turpis.
+          </p>
+          <img src={Logo} draggable="false" alt="capella-logo"/>
+        </div>
+      </div>
+    <footer>
+      <div className="inner-footer">
+        <input type="text" placeholder="Type your question here"/>
+        <button onClick="someFunction();">Submit</button>
+      </div>
+    </footer>
     </div>
-  );
-}
+  </div>
+);
 
-export default App;
+
+export default () => (
+  <div
+    style={{
+      width: '600px',
+      height: '350px',
+    }}
+  >
+    <Rnd
+      default={{
+        x: 150,
+        y: 205,
+        width: 320,
+        height: 500,
+      }}
+      minWidth={320}
+      minHeight={400}
+      bounds="window"
+    >
+      <ChatBox />
+    </Rnd>
+  </div>
+);
